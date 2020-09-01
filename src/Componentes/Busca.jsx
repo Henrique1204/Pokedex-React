1f16cb210183e06dbad950876ddf0b817ffc1125
Paulo <pauloh16.pdr@gmail.com>
2020-09-01 10:43:56 -0300

    Refatorar funcao de buscar pokemon

--- src/Componentes/Busca.jsx
+++ src/Componentes/Busca.jsx
@@ -3,6 +3,7 @@ import "../css/Busca.css";
 
 export default (props) => (
     <div className="centralizar buscaContainer">
-        <input type="number" className="col-4" min="1" max="151" onKeyPress={props.evento} placeholder="#001"/>
+        <input type="number" className="col-4" min="1" max="151" onChange={props.eventoChange} onKeyPress={props.eventoPrincipal} placeholder="#001"/>
+        <button type="button" className="col-2" onClick={props.eventoPrincipal}>Buscar</button>
     </div>
 );
--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -3,41 +3,49 @@ import Busca from "./Busca";
 import "../css/Card.css";
 
 export default () => {
+    const [valor, setValor] = useState("");
     const [titulo, setTitulo] = useState("Buscar Pokemon");
     const [caminho, setCaminho] = useState("img/nao-encontrado.png");
     const [altImg, setAltImg] = useState("Pokémon não encontrado");
     const [legenda, setLegenda] = useState("Busque pelos primeiros #151 Pokémons!");
 
-    async function buscarPokemon(evento) {
+    function renderizarPokemon(pokemon) {
+        setTitulo(pokemon.name.replace("-", " "));
+        setCaminho(`img/${pokemon.name}.png`);
+        setAltImg(pokemon.name);
+
+        const tipos = pokemon.types.map((type) => type.type.name);
+
+        if (tipos.length === 1) {
+            setLegenda(
+                <span className="tipos">
+                    <span className={tipos[0]}>{tipos[0]}</span>
+                </span>
+            );
+        } else {
+            setLegenda(
+                <span className="tipos">
+                    <span className={tipos[0]}>{`${tipos[0]} `}</span>|
+                    <span className={tipos[1]}>{` ${tipos[1]}`}</span>
+                </span>
+            );
+        }
+    }
+
+    async function fetchPokemon(id) {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokemon = await res.json();
+        renderizarPokemon(pokemon);
+    }
+
+    function atualizarValor(evento) {
+        setValor(evento.target.value);
+    }
+
+    function buscarPokemon(evento) {
         try {
-            const valor = evento.target.value;
-            const tecla = evento.key;
-
-            if (valor > 0 && valor < 152 && tecla === "Enter") {
-                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${valor}`);
-                const pokemon = await res.json();
-
-                setTitulo(pokemon.name.replace("-", " "));
-                setCaminho(`img/${pokemon.name}.png`);
-                setAltImg(pokemon.name);
-
-        
-                const tipos = pokemon.types.map((type) => type.type.name);
-
-                if (tipos.length === 1) {
-                    setLegenda(
-                        <span className="tipos">
-                            <span className={tipos[0]}>{tipos[0]}</span>
-                        </span>
-                    );
-                } else {
-                    setLegenda(
-                        <span className="tipos">
-                            <span className={tipos[0]}>{`${tipos[0]} `}</span>|
-                            <span className={tipos[1]}>{` ${tipos[1]}`}</span>
-                        </span>
-                    );
-                }
+            if ((valor > 0 && valor < 152 && evento.key === "Enter") || evento.type === "click") {
+                fetchPokemon(valor);
             }
         } catch (erro) {
             alert(erro);
@@ -53,7 +61,7 @@ export default () => {
             </div>
             
             <p>{legenda}</p>
-            <Busca evento={buscarPokemon} />
+            <Busca eventoChange={atualizarValor} eventoPrincipal={buscarPokemon} />
         </div>
     );
 }
