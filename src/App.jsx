f221689db78bad7a1797024da9d1d145c8576b15
Paulo <pauloh16.pdr@gmail.com>
2020-09-01 11:36:46 -0300

    Adicionar componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment} from "react";
 import Header from "./Componentes/Header";
 import Main from "./Componentes/Main";
+import Footer from "./Componentes/Footer";
 
 export default () => {
     return (
         <Fragment>
             <Header />
             <Main />
+            <Footer />
         </Fragment>
     );
 }
---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../css/Footer.css";
+
+export default () => {
+    return (
+        <footer>
+            <h1>Workshop React - SENAI Suiço Brasileira</h1>
+            <ul className="container">
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+                <li className="col-4"><span>Paulo Henrique</span> <a href="https://github.com/henrique1204"><span>Henrique1204</span></a></li>
+            </ul>
+        </footer>
+    )
+}
