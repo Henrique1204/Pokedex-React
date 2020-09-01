1479274241d35a49b0f8b20b78041091d522df91
José Carlos <jjc.silva10@gmail.com>
2020-09-01 19:21:07 -0300

    Adicionar conteúdo do componente Pokedex

--- src/Componentes/Pokedex.jsx
+++ src/Componentes/Pokedex.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Card from "./Card";
 
 export default () => {
     return (
-        <h1>OI Pokedex</h1>
+        <main>
+            <h1>Pokedex 1° geração</h1>
+            <ul>
+                <li>
+                    <Card isHome={false}
+                        titulo="teste"
+                        caminho="img/pikachu.png"
+                        altImg="teste"
+                        legenda="Teste"/>
+                </li>
+            </ul>
+        </main>
     )
 }
