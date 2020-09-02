18c84433d7030a29a991665e87353ff502c6cc62
Paulo <pauloh16.pdr@gmail.com>
2020-09-02 13:31:57 -0300

    Corrigir erros no carregamento da pokedex

--- src/Componentes/Pokedex.jsx
+++ src/Componentes/Pokedex.jsx
@@ -7,43 +7,42 @@ export default class Pokedex extends Component  {
         pokemons: []
     }
 
-    componentDidMount() {
+    componentDidMount = async () => {
         const numeroDex = 151;
 
         for (let i = 1; i <= numeroDex; i++) {
-            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-            fetch(url).then((res) => res.json())
-            .then((json) => {
-                const tipos = json.types.map((type) => type.type.name);
-                let tiposSpan;
+            const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            const json = await req.json();
         
-                if (tipos.length === 1) {
-                    tiposSpan = (
-                        <span className="tipos">
-                            <span className={tipos[0]}>{tipos[0]}</span>
-                        </span>
-                    );
-                } else {
-                    tiposSpan = (
-                        <span className="tipos">
-                            <span className={tipos[0]}>{`${tipos[0]} `}</span>|
-                            <span className={tipos[1]}>{` ${tipos[1]}`}</span>
-                        </span>
-                    );
-                }
+            const tipos = json.types.map((type) => type.type.name);
+            let tiposSpan;
+    
+            if (tipos.length === 1) {
+                tiposSpan = (
+                    <span className="tipos">
+                        <span className={tipos[0]}>{tipos[0]}</span>
+                    </span>
+                );
+            } else {
+                tiposSpan = (
+                    <span className="tipos">
+                        <span className={tipos[0]}>{`${tipos[0]} `}</span>|
+                        <span className={tipos[1]}>{` ${tipos[1]}`}</span>
+                    </span>
+                );
+            }
 
-                const pokemon = {
-                    nome: json.name,
-                    id: json.id,
-                    numero: `#${json.id.toString().padStart(3, '0')}`,
-                    tipos: tiposSpan
-                }
+            const pokemon = {
+                nome: json.name,
+                id: json.id,
+                numero: `#${json.id.toString().padStart(3, '0')}`,
+                tipos: tiposSpan
+            }
 
-                const pokemons = [...this.state.pokemons];
-                pokemons.push(pokemon);
+            const pokemons = [...this.state.pokemons];
+            pokemons.push(pokemon);
 
-                this.setState({ pokemons });
-            });
+            this.setState({ pokemons });
         }
     };
 
@@ -52,7 +51,7 @@ export default class Pokedex extends Component  {
             <main className="Pokedex">
                 <h1>Pokedex 1º Geração</h1>
                 <ul className="centralizar lista">
-                { this.state.pokemons.sort((a, b) => a - b).map((item) => {
+                { this.state.pokemons.map((item) => {
                         return (
                             <li key={item.id}>
                                 <Card isHome={false} titulo={item.nome} caminho={`img/${item.nome}.png`} altImg={item.nome} numero={item.numero} legenda={item.tipos} />
