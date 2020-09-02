be83a6149500547ea682dd063b1f6ad4d0112ccd
Paulo <pauloh16.pdr@gmail.com>
2020-09-01 21:24:41 -0300

    Adicioanr estilo da pokedex

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -3,15 +3,8 @@ import Card from "./Card";
 
 export default () => {
     return (
-        // Aqui eu só criando um elemento html main, main significa o conteúdo principal do site, o mais relevante, dentro dele tô colocando o card com o pokemon e o campo de busca
         <main className="container centralizar">
-            {/* Aqui tô colocando o card dentro do main*/}
-            <Card isHome={true} 
-                titulo={"Buscar Pokémon"} 
-                caminho={"img/nao-encontrado.png"} 
-                altImg={"Pokémon não encontrado"}
-                legenda={"Busque pelos primeiros #151 Pokémons!"}
-                classes={"col-6"}/>
+            <Card isHome={true} titulo={"Buscar Pokémon"} caminho={"img/nao-encontrado.png"} altImg={"Pokémon não encontrado"} legenda={"Busque pelos primeiros #151 Pokémons!"} classes={"col-6"}/>
         </main>
     );
 }
--- src/Componentes/Pokedex.jsx
+++ src/Componentes/Pokedex.jsx
@@ -5,55 +5,28 @@ import "../css/Pokedex.css";
 export default () => {
     return (
         <main className="Pokedex">
+            <h1>Pokedex 1º Geração</h1>
             <ul className="centralizar lista">
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
                 <li>
-                    <Card isHome={false}
-                        titulo="teste"
-                        caminho="img/pikachu.png"
-                        altImg="teste"
-                        legenda="Teste"/>
+                    <Card isHome={false} titulo="teste" caminho="img/pikachu.png" altImg="teste" legenda="Teste"/>
                 </li>
             </ul>
         </main>
