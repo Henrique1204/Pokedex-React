3bb28f27c6c1bf5da9714dea91595fd51e607266
Paulo <pauloh16.pdr@gmail.com>
2020-09-04 00:27:53 -0300

    Refatorar busca

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -21,38 +21,42 @@ export default class Card extends Component {
     }
 
     fetchPokemon = async () => {
-        for(let i = 1; i <= 151; i++) {
-            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-            const json = await res.json();
-    
-            const tipos = json.types.map((type) => type.type.name);
-            let tiposSpan;
-    
-            if (tipos.length === 1) {
-                tiposSpan = (
-                    <span className="tipos">
-                        <span className={tipos[0]}>{tipos[0]}</span>
-                    </span>
-                );
-            } else {
-                tiposSpan = (
-                    <span className="tipos">
-                        <span className={tipos[0]}>{`${tipos[0]} `}</span>|
-                        <span className={tipos[1]}>{` ${tipos[1]}`}</span>
-                    </span>
-                );
+        try {
+            for(let i = 1; i <= 151; i++) {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+                const json = await res.json();
+        
+                const tipos = json.types.map((type) => type.type.name);
+                let tiposSpan;
+        
+                if (tipos.length === 1) {
+                    tiposSpan = (
+                        <span className="tipos">
+                            <span className={tipos[0]}>{tipos[0]}</span>
+                        </span>
+                    );
+                } else {
+                    tiposSpan = (
+                        <span className="tipos">
+                            <span className={tipos[0]}>{`${tipos[0]} `}</span>|
+                            <span className={tipos[1]}>{` ${tipos[1]}`}</span>
+                        </span>
+                    );
+                }
+        
+                const pokemon = {
+                    nome: json.name,
+                    id: json.id,
+                    numero: `#${json.id.toString().padStart(3, '0')}`,
+                    tipos: tiposSpan
+                }
+        
+                const pokemons = [...this.state.pokemons];
+                pokemons.push(pokemon);
+                this.setState({ pokemons });
             }
-    
-            const pokemon = {
-                nome: json.name,
-                id: json.id,
-                numero: `#${json.id.toString().padStart(3, '0')}`,
-                tipos: tiposSpan
-            }
-    
-            const pokemons = [...this.state.pokemons];
-            pokemons.push(pokemon);
-            this.setState({ pokemons });
+        } catch (erro) {
+            alert(erro);
         }
     }
 
@@ -86,16 +90,19 @@ export default class Card extends Component {
 
     atualizarValor = (evento) => {
         const valor = evento.target.value;
-        /* Isso aqui é uma validação pra ver se alguém tá digitando número maior que 151
+
+        /* 
+        * Isso aqui é uma validação pra ver se alguém tá digitando número maior que 151
         *
         * valor.replace("#", "") -- Primeiro limpo o "#" do número, pra caso alguém digite #025
         *
         * Number(valor.replace("#", "")) -- Segundo converto o valor limpo pra Number
         * isso vai limpar os 0 na esquerda
         */
-       const numero = Number(valor.replace("#", ""));
+        const numero = Number(valor.replace("#", ""));
     
-        /* isNaN(numero) -- Terceiro eu vejo se o const numero vai ser igual a "Not a Number"
+        /*
+        * isNaN(numero) -- Terceiro eu vejo se o const numero vai ser igual a "Not a Number"
         * No caso letra não é número, então da true, numero dá falso
         * 
         * !isNaN(numero) -- Depois eu inverto o valor com "!"
@@ -104,7 +111,6 @@ export default class Card extends Component {
         * Caso dê falso é por que alguém tá digitando uma palavra, então cai no else
         */
         if (!isNaN(numero)) {
-            // Se o numero estiver entre 0 e 151 ele alterar o valor, se não mantém
             if (numero >= 0 && numero < 152) {
                 this.setState({ valor });
             }
@@ -113,49 +119,50 @@ export default class Card extends Component {
         }
     }
 
-    buscarPokemon = (evento) => {
-        try {
-            // Limpo o valor digitado pra ficar sem "#" no número e sem espaço no começo
-            const valor = this.state.valor.replace("#", "").trim();
+    buscarPokemon = (valor) => {
+        // Remove a classe de erro e escreve a mensagem padrão
+        const classes = this.state.classes.replace("erro", "");
+        this.setState({ classes, legenda: "Busque pelos primeiros #151 Pokémons!" });
 
-            // Testo se alguém apertou "Enter" ou se clicou no botão
-            if(evento.key === "Enter" || evento.type === "click") {
-                // Remove a classe de erro e escreve a mensagem padrão
-                const classes = this.state.classes.replace("erro", "");
-                this.setState({ classes, legenda: "Busque pelos primeiros #151 Pokémons!" });
+        /*
+        * Busca (find) na lista dos pokémons um pokémon que tenha
+        * Ou o id igual ao valor convertido para número
+        * Ou o nome sem "-" e em minúsculo igual ao valor em minúsculo
+        * Caso um dos casos acima seja verdadeiro ele retorna o pokemon
+        * Caso todos sejam falso ele retorn undefined
+        */
+        const pokemon = this.state.pokemons.find((item) => (
+            item.id === Number(valor) ||
+            item.nome.replace("-", " ").toLowerCase() === valor.toLowerCase()
+        ));
 
-                /* Busca (find) na lista dos pokémons um pokémon que tenha
-                * Ou o id igual ao valor convertido para número
-                * Ou o nome sem "-" e em minúsculo igual ao valor em minúsculo
-                * Caso um dos casos acima seja verdadeiro ele retorna o pokemon
-                * Caso todos sejam falso ele retorn undefined
-                */
-                const pokemon = this.state.pokemons.find((item) => (
-                    item.id === Number(valor) ||
-                    item.nome.replace("-", " ").toLowerCase() === valor.toLowerCase()
-                ));
+        /*
+        * Valor undefined é igual a false
+        * então se o find retornar undefined ele cai no else
+        */
+        if (pokemon) {
+            this.renderizarPokemon(pokemon);
+        } else {
+            // Adiciona a classe de erro e troca os valores dos elementos
+            const classes = `${this.state.classes} erro`;
+            this.setState({
+                titulo: "Buscar Pokémons",
+                caminho: "img/nao-encontrado.png",
+                altImg: "Pokémon não encontrado",
+                legenda: "Valor digitado não é válido!",
+                numero: "???",
+                classes
+            });
+        }
+    }
 
-                /* Valor undefined é igual a false
-                * então se o find retornar undefined ele cai no else
-                */
-                if (pokemon) {
-                    // Se tiver encontrado o pokemon ele renderiza ele no card
-                    this.renderizarPokemon(pokemon);
-                } else {
-                    // Adiciona a classe de erro e troca os valores dos elementos
-                    const classes = `${this.state.classes} erro`;
-                    this.setState({
-                        titulo: "Buscar Pokémons",
-                        caminho: "img/nao-encontrado.png",
-                        altImg: "Pokémon não encontrado",
-                        legenda: "Valor digitado não é válido!",
-                        numero: "???",
-                        classes
-                    });
-                }
-            }
-        } catch (erro) {
-            alert(erro);
+    eventoBusca = (evento) => {
+        // Limpo o valor digitado pra ficar sem "#" no número e sem espaço no começo
+        const valor = this.state.valor.replace("#", "").trim();
+
+        // Testo se alguém apertou "Enter" ou se clicou no botão
+        if(evento.key === "Enter" || evento.type === "click") {
+            this.buscarPokemon(valor);
         }
     }
 
@@ -178,7 +185,7 @@ export default class Card extends Component {
     
                 <p className="legenda">{this.state.legenda}</p>
                 <Condicional condicao={this.state.isHome}>
-                    <Busca valor={this.state.valor} eventoChange={this.atualizarValor} eventoPrincipal={this.buscarPokemon} />
+                    <Busca valor={this.state.valor} eventoChange={this.atualizarValor} eventoPrincipal={this.eventoBusca} />
                 </Condicional>
             </div>
         );
