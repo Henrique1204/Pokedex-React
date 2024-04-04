8a95a9b59e2e25045fc9c3091191614c516095a5
Paulo <pauloh16.pdr@gmail.com>
2024-04-03 21:09:11 -0300

    chore: Configurar typescript e babel

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "transform-inline-environment-variables",
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "Mocks/*": "./src/@mocks/*",
+          "Types/*": "./src/@types/*",
+          "Components/*": "./src/Components/*",
+          "Core/*": "./src/Core/*",
+          "Assets/*": "./src/Core/Assets/*",
+          "Constants/*": "./src/Core/Constants/*",
+          "Helpers/*": "./src/Core/Helpers/*",
+          "Services/*": "./src/Core/Services/*",
+          "Hooks/*": "./src/Hooks/*",
+          "Contexts/*": "./src/Contexts/*",
+          "Pages/*": "./src/Pages/*",
+          "Tests/*": "./src/Tests/*",
+        },
+      },
+    ],
+  ],
+};
